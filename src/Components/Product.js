5743a1b7a100f3368c740b5c85ee2f793d02216e
Remi-dee <oluwaremidaniel36@gmail.com>
2023-04-10 04:09:14 -0700

    Homepage fix

---
+++ src/Components/Product.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { StarIcon } from "@heroicons/react/24/solid";
+import Currency from "react-currency-formatter";
+
+function Product({ id, title, price, description, category, image }) {
+  const MAX_RATING = 5;
+  const MIN_RATING = 1;
+  const [rating, setRating] = useState(0);
+  const [hasPrime, setHasPrime] = useState(true);
+
+  useEffect(() => {
+    setRating(
+      Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+    );
+    setHasPrime(Math.random() < 0.5);
+  }, []);
+
+  return (
+    <div className="relative flex flex-col  m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400 ">
+        {category}
+      </p>
+      <Image
+        src={image}
+        height={200}
+        width={200}
+        style={{ objectFit: "contain" }}
+        alt="Product"
+      />
+      <h4 className="my-3">{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <div className="mb-5">
+        <Currency quantity={price} currency="GBP" />
+      </div>
+
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img className="w-12" src="https://links.papareact.com/fdw" alt="Prime" />
+          <p className="text-xs text-gray-500">Free Next-day Delivery</p>
+        </div>
+      )}
+
+      <button className="mt-auto button">Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
--- src/Components/ProductZone.js
+++ src/Components/ProductZone.js
@@ -1,13 +1,45 @@
-function ProductZone({products}) {
+import Product from "./Product";
+
+function ProductZone({ products }) {
   return (
-    <div>
-       <h1>Product </h1>
-     {products.map((product) => (
-      <p>{product.title}</p>
-     ))}
-      
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt="Advert"
+      />
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 md:col-span-full">
+        {products
+          .slice(4, 8)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+
+     
     </div>
-  )
+  );
 }
 
-export default ProductZone
+export default ProductZone;

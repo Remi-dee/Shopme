f871f7c6b5ebea89dea72c22a9d94f21f9adf0d9
Remi-dee <oluwaremidaniel36@gmail.com>
2023-04-20 12:53:56 -0700

    file case fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
-import { store } from "../app/store";
+import { store } from "../lib/store";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({

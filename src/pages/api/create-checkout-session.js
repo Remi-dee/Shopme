1c031715c27e4d0591ec7700068bc54c658152df
Remi-dee <oluwaremidaniel36@gmail.com>
2023-04-23 16:22:34 -0700

    stripe checkout integration

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,27 +1,51 @@
-const stripe = require("stripe")("process.env.STRIPE_SECRET_KEY");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
   const { items, email } = req.body;
 
   const transformedItems = items.map((item) => ({
-    description: item.description,
-    quantity: 1,
     price_data: {
       currency: "usd",
       unit_amount: item.price * 100,
       product_data: {
         name: item.title,
+        description: item.description,
         images: [item.image],
       },
     },
+    quantity: 1,
   }));
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    shipping_rates: ["shr_1MzMNHBrzRru3xbcclQkFRzS"],
-    shipping_address_collection: {
-      allowed_countries: ["US", "UK", "CA"],
+    shipping_address_collection: { allowed_countries: ["US", "CA", "NI"] },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: { amount: 0, currency: "usd" },
+          display_name: "Free shipping",
+          delivery_estimate: {
+            minimum: { unit: "business_day", value: 5 },
+            maximum: { unit: "business_day", value: 7 },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: { amount: 1500, currency: "usd" },
+          display_name: "Next day air",
+          delivery_estimate: {
+            minimum: { unit: "business_day", value: 1 },
+            maximum: { unit: "business_day", value: 1 },
+          },
+        },
+      },
+    ],
+    phone_number_collection: {
+      enabled: true,
     },
+
     line_items: transformedItems,
     mode: "payment",
     success_url: `${process.env.HOST}/success`,
@@ -31,7 +55,6 @@ export default async (req, res) => {
       images: JSON.stringify(items.map((item) => item.image)),
     },
   });
-
   res.status(200).json({ id: session.id });
 };
 

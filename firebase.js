252af0528a06fb349e3b95e9042ce71fda6a001f
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-05 00:40:55 +0100

    final update

--- firebase.js
+++ firebase.js
@@ -1,20 +1,24 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAlcVRlMqOfdeeLz7SFNn13iKpUcJL-avY",
-  authDomain: "shop-zone-dbd60.firebaseapp.com",
-  projectId: "shop-zone-dbd60",
-  storageBucket: "shop-zone-dbd60.appspot.com",
-  messagingSenderId: "195613922536",
-  appId: "1:195613922536:web:829b238f015d36279b655d",
-  measurementId: "G-TE6TKYTXJR",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
+const app = initializeApp(firebaseConfig);
 
-const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
-
-const db = app.firestore();
+const db = getFirestore(app);
 
 export default db;
 
-
+// const app = !firebase.apps.length
+//   ? firebase.initializeApp(firebaseConfig)
+//   : firebase.app();
--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,22 +1,48 @@
 import Header from "@/Components/Header";
-import { useSession } from "next-auth/react";
+import moment from "moment/moment";
+import { getSession, useSession } from "next-auth/react";
+import db from "../../firebase";
+import {
+  collection,
+  query,
+  orderBy,
+  getDocs,
+  doc,
+  getFirestore,
+} from "firebase/firestore";
+import Order from "@/Components/Order";
 
-function Orders({ orders }) {
-  const { data: session } = useSession();
+function Orders({ orders, session }) {
   return (
     <div>
       <Header />
       <main className="max-w-screen-lg p-10 mx-auto">
-        <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
+        <h1 className="text-3xl border-b mb-2 pb-1 border-orange-400">
           Your Orders
         </h1>
         {session ? (
-          <h2>x orders</h2>
+          <h2>{orders.length} orders</h2>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
 
-        <div className="mt-5 space-y-4"></div>
+        <div className="mt-5 space-y-4">
+          {orders?.map(
+            ({ id, amount, amountShipping, items, timestamp, images }) => (
+              <>
+                <Order
+                  key={id}
+                  id={id}
+                  amount={amount}
+                  amountShipping={amountShipping}
+                  items={items}
+                  timestamp={timestamp}
+                  images={images}
+                />
+              </>
+            )
+          )}
+        </div>
       </main>
     </div>
   );
@@ -24,38 +50,59 @@ function Orders({ orders }) {
 
 export default Orders;
 
-{
-  /*export async function getServerSideProps(context) {
-    const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-    
-    // Get users logged in credentials
-    const session = getSession(context);
+export async function getServerSideProps(context) {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-    if(!session) {
-        return {
-            props: {}
-        };
-    }
+  // Get users logged in credentials
+  const session = await getSession(context);
 
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
 
-    // Firebase db
-    const stripeOrders = await db
-    .collection("users")
-    .doc((await session).user.email)
-    .collection("orders")
-    .orderBy("timestamp", "desc")
-.get();
+  // Firebase db
+  // const stripeOrders = await db
+  //   .collection("users")
+  //   .doc(session.user.email)
+  //   .collection("orders")
+  //   .orderBy("timestamp", "desc")
+  //   .get();
 
-// Stripe orders
+  //const docRef = doc(db, `users/${session.user.email}`);
+  // const colRef = doc(docRef, "orders")
+  // const stripeOrders = await getDoc(docRef);
+  //const stripeOrders = query(docRef, orderBy("timestamp", "desc"));
+  // Stripe orders
 
-const orders =  await Promise.all(
-    stripeOrders.docs.map(async (order) => ({
+  // Assuming stripeOrders is the original query
+  const stripeOrdersRef = collection(db, "users", session.user.email, "orders");
+  const orderedStripeOrdersRef = query(
+    stripeOrdersRef,
+    orderBy("timestamp", "desc")
+  );
 
-
-
-
+  const stripeOrdersSnapshot = await getDocs(orderedStripeOrdersRef);
+  const orders = await Promise.all(
+    stripeOrdersSnapshot.docs.map(async (order) => ({
+      id: order.id,
+      amount: order.data().amount,
+      amountShipping: order.data().amount_shipping,
+      images: order.data().images,
+      timestamp: moment(order.data().timestamp.toDate()).unix(),
+      items: (
+        await stripe.checkout.sessions.listLineItems(order.id, {
+          limit: 100,
+        })
+      ).data,
     }))
-)
+  );
 
-}*/
+  return {
+    props: {
+      orders,
+      session,
+    },
+  };
 }

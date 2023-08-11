09c2aaaa620cf17fc02884f560c6acaee1625fa3
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-08-11 12:29:02 +0100

    updated feedback on filter

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -20,7 +20,7 @@ function Banner() {
             showStatus={false}
             showIndicators={false}
             showThumbs={false}
-            interval={5000}
+             interval={5000}
           >
             <div>
               <Image
